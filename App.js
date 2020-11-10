3fc25fdaf3d04600e424529d5b7e8f8e6b310838
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2020-11-10 10:59:37 +0500

    started class again 10 Nov 2020

--- App.js
+++ App.js
@@ -1,25 +1,114 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./app/Screens/Home";
-import About from "./app/Screens/About";
-
-const StackNav = createStackNavigator();
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default class App extends Component {
+  state = {
+    data: [],
+    Name: "",
+    Age: "",
+  };
+
+  onSubmit = () => {
+    // ... spread operator
+
+    const name = this.state.data.filter(
+      (item) =>
+        item.Name.trim().toLowerCase() == this.state.Name.trim().toLowerCase()
+    );
+
+    if (name.length > 0) {
+      alert("duplicate checked");
+      return;
+    }
+
+    this.setState({
+      data: [
+        ...this.state.data,
+        { Name: this.state.Name, Age: this.state.Age },
+      ],
+      Name: "",
+      Age: "",
+    });
+  };
+
   render() {
     return (
-      <NavigationContainer>
-        <StackNav.Navigator>
-          <StackNav.Screen
-            options={{ headerShown: false }}
-            name="Home"
-            component={Home}
+      <SafeAreaView style={{ marginTop: StatusBar.currentHeight }}>
+        <View style={{ width: "100%", alignItems: "center", marginTop: 20 }}>
+          <TextInput
+            style={{
+              borderWidth: 1,
+              borderColor: "gray",
+              borderStyle: "solid",
+              width: "90%",
+              padding: 10,
+              borderRadius: 10,
+            }}
+            placeholder="Name"
+            value={this.state.Name}
+            onChangeText={(val) => this.setState({ Name: val })}
           />
-          <StackNav.Screen name="About" component={About} />
-        </StackNav.Navigator>
-      </NavigationContainer>
+
+          <TextInput
+            style={{
+              borderWidth: 1,
+              borderColor: "gray",
+              borderStyle: "solid",
+              width: "90%",
+              padding: 10,
+              borderRadius: 10,
+              marginTop: 20,
+            }}
+            keyboardType="numeric"
+            placeholder="Age"
+            value={this.state.Age}
+            onChangeText={(val) => this.setState({ Age: val })}
+          />
+
+          <TouchableOpacity onPress={this.onSubmit}>
+            <View
+              style={{
+                width: 150,
+                height: 40,
+                borderRadius: 10,
+                backgroundColor: "tomato",
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: 20,
+              }}
+            >
+              <Text style={{ color: "white" }}>Submit</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ width: "100%", alignItems: "center", marginTop: 20 }}>
+          {this.state.data.map((item, index) => {
+            return (
+              <View
+                key={index}
+                style={{
+                  width: "90%",
+                  padding: 10,
+                  backgroundColor: "gainsboro",
+                  borderRadius: 10,
+                  marginTop: 10,
+                }}
+              >
+                <Text>Name :- {item.Name}</Text>
+                <Text>Age :- {item.Age}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </SafeAreaView>
     );
   }
 }
