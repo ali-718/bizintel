9471ef332d7c8ba6e8cc23a351d086c41cbe3f6b
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2020-12-07 10:55:33 +0500

    learn navigation today

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 import ApiPractice from "./app/Screens/ApiPractice";
 import Form from "./app/Screens/FormPractice";
+import Router from "./Router";
 
 export default class App extends Component {
   render() {
-    return <ApiPractice />;
+    return <Router />;
   }
 }
---
+++ Router.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./app/Screens/Home";
+import About from "./app/Screens/About";
+
+const Stack = createStackNavigator();
+
+export default class Router extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen name="About" component={About} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
